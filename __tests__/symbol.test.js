16eac27f9d8c150093ffc1b48adbe4cef3e9dbd4
manhvu <manhvu@orgos.net>
2020-10-27 14:43:52 +0700

    feat: :sparkles: convert number to symbol name

---
+++ __tests__/symbol.test.js
@@ -0,0 +1,38 @@
+const { symbol, nameToUint64 } = require('../src');
+
+describe('test symbol name', () => {
+  it('EOS symbol', () => {
+    /**
+     * cleos -u https://eos.greymass.com get scope eosio.token -t stat
+     */
+    const name = '........ehbo5';
+    const uint64 = nameToUint64(name);
+    expect(symbol.toName(uint64)).toEqual('EOS');
+  });
+
+  it('parse symbol name', () => {
+    /**
+     * cleos -u https://api.canfoundation.io get scope canpasstoken -t stat
+     */
+    const data = [
+      {
+        scope: '.11oen2oeh2p4',
+      },
+      {
+        scope: '.1b5glegch2og',
+      },
+      {
+        scope: '.1f4en23cpa54',
+      },
+    ];
+
+    const rs = ['TESTLEC', 'LECLEVN', 'TLECLEV'];
+
+    data
+      .map(({ scope }) => nameToUint64(scope))
+      .map((n) => symbol.toName(n))
+      .forEach((sym, i) => {
+        expect(sym).toEqual(rs[i]);
+      });
+  });
+});
